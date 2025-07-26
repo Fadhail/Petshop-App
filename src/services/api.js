8f17d3ccc04ae9b6e4e3b3cd0be763217d6dd709
Fadhail <mochammadfadhail2@gmail.com>
2025-07-26 21:13:38 +0700

    fix: change api endpoint

--- src/services/api.js
+++ src/services/api.js
@@ -1,13 +1,13 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:3000/api',
+  baseURL: 'https://r8s888o0wos0coc8sc0sg8ok.irc-enter.tech/api',
   timeout: 5000,
 });
 
 // Auth API (tanpa prefix /api)
 const authApi = axios.create({
-  baseURL: 'http://localhost:3000',
+  baseURL: 'https://r8s888o0wos0coc8sc0sg8ok.irc-enter.tech/api',
   timeout: 5000,
 });
 
